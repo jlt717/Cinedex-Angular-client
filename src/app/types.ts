6d27334852d541022b9ed7ea3b06757346e42e00
schnecke <eiweala@live.com>
2023-09-18 07:06:17 -0700

    Add likes and update posts

---
+++ src/app/types.ts
@@ -0,0 +1,38 @@
+export interface UserLoginRequest {
+  Username: string;
+  Password: string;
+}
+
+export interface UserRegRequest extends UserLoginRequest {
+  Email: string;
+  Birthday: Date;
+}
+
+export interface UserLoginResponse extends UserRegRequest {
+    _id: string;
+    FavoriteMovies: Movie[];
+};
+
+export interface UserEditResponse extends UserRegRequest {
+  _id: string;
+  FavoriteMovies: string[];
+};
+
+export interface Movie {
+  _id: string;
+  Description: string;
+  Director: {
+    Name: string;
+    Bio: string;
+    Birthyear: number;
+    Deathyear: number;
+  }
+  Featured:boolean;
+  Genre: {
+    Name: string;
+    Description: string;
+  };
+  ImageURL: string;
+  Released: string;
+  Title: string;
+}
